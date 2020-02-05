7dcbb9c889909ec05123d8de78bf1284bfd2982c
mmakiri <56089506+mmakiri@users.noreply.github.com>
2020-02-05 14:45:32 +0200

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #This script pulls from remote master branch
+cd /home/tiimi07/
 git checkout master
 git add * 
 git stash
 git pull
 git fetch -p origin
 git merge origin/master
+chmod -R 755 *
 echo Done
