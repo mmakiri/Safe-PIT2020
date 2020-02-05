d8689d8eafc457ae63d65c733c0d5ce38e5d57c0
mmakiri <miska.makirinta@edu.turkuamk.fi>
2020-02-05 16:25:08 +0200

    test

--- public_html/assets/js/navbar.js
+++ public_html/assets/js/navbar.js
@@ -1,3 +1,7 @@
+document.getElementById("navbar").addEventListener("click", function() {
+	alert("Hello World!");
+});
+
 function navbar() {
 	var x = document.getElementById("navbar");
 	if (x.style.display === "block") {
