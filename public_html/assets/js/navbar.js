a6d99f34ad8404a190d2b00b35fb43ee5ca4e57a
mmakiri <miska.makirinta@edu.turkuamk.fi>
2020-02-05 16:17:24 +0200

    rollback

--- public_html/assets/js/navbar.js
+++ public_html/assets/js/navbar.js
@@ -1,4 +1,3 @@
-document.addEventListener("click", navbar);
 function navbar() {
 	var x = document.getElementById("navbar");
 	if (x.style.display === "block") {
@@ -6,9 +5,4 @@ function navbar() {
 	} else {
 	  x.style.display = "block";
 	}
-	if (event.target.classList.contains('icon')){
-        dropdown.classList.toggle('show');
-    	} else {
-        dropdown.classList.remove('show');
-    	}
-}
+  }
